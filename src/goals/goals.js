bf21e39ab5577a47fa497adfd9a51ea7a2821110
Fiskell <stephen.fiskell@gmail.com>
2016-08-28 22:27:42 -0700

    Add input with button

--- src/goals/goals.js
+++ src/goals/goals.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 export default React.createClass({
+  getInitialState() {
+    return {goalName: ''};
+  },
   componentDidMount() {
+  },
+  handleAddGoal() {
+    console.log(this.state.goalName);
+  },
+  onChangeValue: function(event) {
+    this.setState(
+    {
+      goalName: event.target.value
+    });
   },
   render() {
     return (
-      <div>hey</div>
+      <div>
+        <input name='goal-name-input' value={this.state.goalName} onChange={this.onChangeValue}/>
+        <input onClick={this.handleAddGoal} type='button' value="Add"/>
+      </div>
     );
   }
 })
